94bdcc9fc3cd6a7837caaee09a5bf22e8a74ce34
Swaminathan Vetri <svswaminathan@gmail.com>
2021-01-16 11:00:12 +0530

    Added InputFile Demo

---
+++ Models/ImageProperties.cs
@@ -0,0 +1,10 @@
+using System.Diagnostics;
+
+namespace dotnet_conf_2020_demo
+{
+    public class ImageProperties
+    {
+        public int MaxWidth { get; set; }
+        public int MaxHeight { get; set; }
+    }
+}
